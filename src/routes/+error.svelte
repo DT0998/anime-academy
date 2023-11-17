5c15a2a7b8e863fbc1177bad8d3875d2bcb7da30
DT0998 <duongkytrieuvn@gmail.com>
2023-11-18 01:07:19 +0700

    feat: add smth

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import img404 from '$assets/img/404/404.png';
+	import NavBar from '$components/NavBar/NavBar.svelte';
 	import Footer from '$components/Footer/Footer.svelte';
-	import Navbar from '$components/NavBar/NavBar.svelte';
 </script>
 
 <div class="flex flex-col min-h-screen justify-between">
-	<Navbar extraClassInner="justify-center" />
+	<NavBar extraClassInner="justify-center" />
 	<div class="flex justify-center items-center flex-col flex-1">
 		<img src={img404} alt="404" class="max-w-full" />
 		<div class="py-[30px]">
